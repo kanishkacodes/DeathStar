3d5f7512f83422249fbfca50a1e727f8d9206fd4
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2023-03-27 11:14:58 +0530

    version-2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,27 +3,30 @@ import React from 'react';
 export default function Navbar() {
   return (
     <>
-      <div>
+      <div className="border-spacing-3 p-4 flex border-b mb-4">
+        <div className="flex space-x-4">
         <div>
           <img  src="https://www.practo.com/nav/9.5.15/consumer/images/practo.svg" alt="Practo logo" />
         </div>
-        <div >
+        <div>
           <a href="/">Find Doctors</a>
           <a href="/" >Video Consult</a>
           <a href="/" >Medicines</a>
           <a href="/" >Labtests</a>
           <a href="/">Surgeries</a>
         </div>
-        <div>
+        </div>
+        
+        <div className='flex right-0'>
           <a href="/"><span>new</span> For Corporates</a>
           <a href="/">For Providers</a>
           <a href="/">Security &amp; Help</a>
-        </div>
-        <div>
+          <div>
           <button className="bg-white hover:text-blue-300 hover:border-blue-300 text-gray-800  py-2 px-2 pr-3 pl-3 border border-gray-400 rounded text-xs">Login / Signup</button>
         </div>
+        </div>
+
       </div>
-      <hr className="border-t-2 border-gray-200 my-4" />
     </>
   );
 }
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -3,23 +3,23 @@ import React from 'react'
 export default function Searchbar() {
   return (
     <>
-      <div className="flex items-center border border-gray-300 rounded-md px-2">
-        <div className="flex items-center justify-center h-10 w-10 border-r border-gray-300">
+      {/* <div>
+        <div >
           <img src="images\location-pin-svgrepo-com.svg" alt='' />
         </div>
-        <input type="text" placeholder="Search locations" className="w-full focus:outline-none" />
+        <input type="text" placeholder="Search locations" />
       </div>
-      <div className="flex items-center border border-gray-300 rounded-md mt-2 px-2">
-        <div className="flex items-center justify-center h-10 w-10 border-r border-gray-300">
+      <div>
+        <div >
           <img src="images\search-svgrepo-com.svg" alt='' />
         </div>
-        <input type="text" placeholder="Search for doctors, clinics, hospitals, etc." className="w-full focus:outline-none" />
+        <input type="text" placeholder="Search for doctors, clinics, hospitals, etc." />
       </div>
-      <div className="flex items-center mt-2">
-        <p className="mr-1">Try</p>
-        <span className="bg-blue-500 text-white px-1 rounded-md mr-1">Plus</span>
+      <div>
+        <p>Try</p>
+        <span>Plus</span>
         <p>Save upto 60% on your healthcare expanses</p>
-      </div>
+      </div> */}
     </>
   )
 }
