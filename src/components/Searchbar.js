86df7faa2791ce5814913fa466a2421a909d6dce
Kanishka Kumar <99749556+kanishkacodes@users.noreply.github.com>
2023-04-04 22:29:14 +0530

    v-2.1

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -22,10 +22,10 @@ export default function Searchbar() {
         </div>
       </div>
       </div>
-      <div className=' flex  ml-auto  bg-pink-200 mr-20'>
-        <p className=''>Try</p>
-        <span>Plus</span>
-        <p>Save upto 60% on your healthcare expanses</p>
+      <div className=' flex  ml-auto  bg-pink-200 mr-20 text-xs p-3 '>
+        <p className=' font-semibold'>Try</p>
+        <span className=' p-1 bg-pink-600 text-sm text-white '>Plus</span>
+        <p>Save upto 60% on your healthcare expanses </p>
       </div>
       </div>
     </>
